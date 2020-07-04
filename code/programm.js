f9199c3dde83018dccce2f010a15927047cf4745
Generaluserup <47183629+Generaluserup@users.noreply.github.com>
2020-07-04 10:18:41 +0300

    js

--- code/programm.js
+++ code/programm.js
@@ -8,12 +8,3 @@ menu.onclick = function myFunction() {
         x.className = "topnav";
     }
 }
-
-
-
-$(function() {
-	
-	$("h2").wrapInner("<span>");
-	$("h2 br").before("<span class='spacer'>")
-		.after("<span class='spacer'>");
-});
